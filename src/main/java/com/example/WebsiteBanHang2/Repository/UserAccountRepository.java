7b5fc21dc7410f79020b2727c07a9f570258c40c
Nguyễn Tú <nguyendes1410@gmail.com>
2025-03-23 11:55:01 +0700

    update

--- src/main/java/com/example/WebsiteBanHang2/Repository/UserAccountRepository.java
+++ src/main/java/com/example/WebsiteBanHang2/Repository/UserAccountRepository.java
@@ -3,7 +3,9 @@ package com.example.WebsiteBanHang2.Repository;
 import com.example.WebsiteBanHang2.Dto.UserAccountDTO;
 import com.example.WebsiteBanHang2.Model.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserAccountRepository extends JpaRepository<UserAccount, Integer> {
     UserAccount findByEmail(String email);
 }
